f441cdb29c614b81a4a1425ab1fe4d0973190bf3
Priyanshi Verma <priyanshiv.hestabit@gmail.com>
2026-02-11 15:33:45 +0530

    Modified healthcheck.sh to ping every 1 s and added screenshot of scheduled cron job

--- Week 1/Day 5/healthcheck.sh
+++ Week 1/Day 5/healthcheck.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-SERVER="8.8.8.8"
+SERVER="192.0.2.1"
 LOG_FILE="logs/health.log"
 TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
 
-ping -c 1 $SERVER > /dev/null 2>&1
+while true
+do
+  TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
 
-if [ $? -ne 0 ]; then
-  echo "[$TIMESTAMP] Server unreachable" >> $LOG_FILE
-else
-  echo "[$TIMESTAMP] Server is healthy" >> $LOG_FILE
-fi
+  if ! ping -c 1 $SERVER > /dev/null 2>&1
+  then
+    echo "[$TIMESTAMP] Server unreachable" >> $LOG_FILE
+  fi
+
+  sleep 1
+done
